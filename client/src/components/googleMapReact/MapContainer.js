2c2f37f02baa0dbae90dd6df5f2e441f8888255c
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-02-13 11:48:41 -0800

    trying to make map marker fit on galaxy s9+

--- client/src/components/googleMapReact/MapContainer.js
+++ client/src/components/googleMapReact/MapContainer.js
@@ -344,7 +344,7 @@ const MapContainer = () => {
             // lng: originalCoordinates.lng - 0.021,
             // Fully in view
             // lng: originalCoordinates.lng - 0.012,
-            lng: originalCoordinates.lng - 0.0085
+            lng: originalCoordinates.lng - 0.0065
 
         };
         setCentered(adjustedCoordinates);
