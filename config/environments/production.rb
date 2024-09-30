877b2765cec0af9cc7ec5bc86c9a084f3ab273ed
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-09-30 14:01:32 -0700

    added ref to contact chatbox

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,49 +1,56 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
-
-  # Redirect non-www to www
-  config.middleware.insert_before 0, Rack::Rewrite do
-    r301 %r{.*}, 'https://www.laorthos.com$&', if: Proc.new { |rack_env|
-      rack_env['SERVER_NAME'] == 'laorthos.com'
-    }
-  end
 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot.
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
+  config.serve_static_assets = true
+  config.assets.digest = true
   config.action_controller.perform_caching = true
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # config.require_master_key = true
 
-  # Ensure static assets are served when configured
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['HEROKU']
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.public_file_server.headers = {
     'Cache-Control' => 'public, max-age=31536000'
   }
-
-  # Set consistent asset host to avoid mismatches
-  config.asset_host = "https://www.laorthos.com"
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.asset_host = "http://laorthos.com"
   config.hosts << "laorthos.com"
-  config.hosts << "www.laorthos.com"
+config.hosts << "www.laorthos.com"
 
   config.assets.compile = false
 
-  # Active Storage configuration
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
+  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
   config.active_storage.service = :local
 
-  # Force all access to the app over SSL
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # Log level and tags
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
   config.log_level = :info
+
+  # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  # Use a different cache store in production
+  # Use a different cache store in production.
   config.cache_store = :redis_cache_store, { url: ENV['REDIS_TLS_URL'], expires_in: 30.days }
   config.active_job.queue_adapter = :sidekiq
 
@@ -55,17 +62,23 @@ Rails.application.configure do
     config.redis = { url: ENV['REDIS_URL'] }
   end
 
-  # Enable locale fallbacks
+  # config.cache_store = :mem_cache_store
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  # Don't log any deprecations
+  # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Default logging formatter
+  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Action Mailer configuration
+  # Use a different logger for distributed setups.
+  # require "syslog/logger"
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
   config.action_mailer.asset_host = "https://www.laorthos.com"
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -76,14 +89,12 @@ Rails.application.configure do
     authentication: 'plain',
     enable_starttls_auto: true
   }
-
-  # Logger configuration
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations
+  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
