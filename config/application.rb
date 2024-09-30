13575c9800ce8a374220ee636c4774c71f83f7ea
James Lagattuta <jimmy.lagattuta@gmail.com>
2024-09-30 14:38:02 -0700

    added ref to contact chatbox

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module LaOrthos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
   
-    config.middleware.use "ForceWWW"
+    config.middleware.use ForceWWW
 
     # Configuration for the application, engines, and railties goes here.
     #
